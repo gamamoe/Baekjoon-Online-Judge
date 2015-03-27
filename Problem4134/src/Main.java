7f694a8ab83f5fac33f6dd06e7f149e8ea5851ec
Geunho Lee <lkh7689@gmail.com>
2015-03-27 16:13:02 +0900

    Accept

--- Problem4134/src/Main.java
+++ Problem4134/src/Main.java
@@ -11,9 +11,9 @@ public class Main {
       long testValue = scanner.nextLong();
       BigInteger nextPrimeNumber = new BigInteger(String.valueOf(testValue));
       
-      if (nextPrimeNumber.isProbablePrime(1)) {
+      if (nextPrimeNumber.isProbablePrime(10)) {  // 입력 값이 소수면 그대로 출력 
         System.out.println(nextPrimeNumber);
-      } else {
+      } else {  // 입력 값이 소수가 아니면 다음 소수를 구해서 출력
         System.out.println(nextPrimeNumber.nextProbablePrime());
       }
     }
