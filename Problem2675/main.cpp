1c8516d3306db6b45ea662f8f7b2a77b50b15525
Geunho Lee <lkh7689@gmail.com>
2015-05-01 17:28:22 +0900

    Accept

---
+++ Problem2675/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int main() {
+  int num_of_test_cases = 0;
+
+  scanf(" %d", &num_of_test_cases);
+  for (int i = 0; i < num_of_test_cases; ++i) {
+    int num_of_repeat = 0;
+    std::string string_sequence;
+
+    std::cin >> num_of_repeat >> string_sequence;
+    for (const auto& c : string_sequence) {
+      for (int j = 0; j < num_of_repeat; ++j) {
+        printf("%c", c);
+      }
+    }
+    printf("\n");
+  }
+}
